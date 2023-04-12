858adc1608fd85246585a9ffb070655705d6337f
pandasBrain <yash48237@gmail.com>
2023-04-12 05:59:49 -0600

    Setup

---
+++ click_count/click_count_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ClickCountApiConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "click_count_api"
---
+++ click_count/click_count_api/models.py
@@ -0,0 +1,10 @@
+import uuid
+from django.db import models
+
+# Create your models here.
+class ClickCount(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    city = models.CharField(max_length=200)
+    country = models.CharField(max_length=200)
+    count = models.IntegerField(default=0)
+
